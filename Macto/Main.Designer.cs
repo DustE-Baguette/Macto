e00d64e629d0cc70a04e38109cbe2bc535f600e2
Dominykas Jaksta <domjaksta@gmail.com>
2025-07-06 21:55:09 +0100

    Added KeyHook library and setup initial button selection features

--- Macto/Main.Designer.cs
+++ Macto/Main.Designer.cs
@@ -28,20 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.macroBTN = new System.Windows.Forms.Button();
+            this.macroSelectionTB = new System.Windows.Forms.TextBox();
+            this.macroSelectionLBL = new System.Windows.Forms.Label();
+            this.pressKeyLBL = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // macroBTN
+            // 
+            this.macroBTN.BackColor = System.Drawing.Color.DimGray;
+            this.macroBTN.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.macroBTN.ForeColor = System.Drawing.Color.White;
+            this.macroBTN.Location = new System.Drawing.Point(241, 360);
+            this.macroBTN.Name = "macroBTN";
+            this.macroBTN.Size = new System.Drawing.Size(80, 30);
+            this.macroBTN.TabIndex = 0;
+            this.macroBTN.Text = "Select";
+            this.macroBTN.UseVisualStyleBackColor = false;
+            this.macroBTN.Click += new System.EventHandler(this.macroBTN_Click);
+            // 
+            // macroSelectionTB
+            // 
+            this.macroSelectionTB.BackColor = System.Drawing.Color.Gray;
+            this.macroSelectionTB.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.macroSelectionTB.ForeColor = System.Drawing.Color.White;
+            this.macroSelectionTB.Location = new System.Drawing.Point(197, 311);
+            this.macroSelectionTB.Name = "macroSelectionTB";
+            this.macroSelectionTB.ReadOnly = true;
+            this.macroSelectionTB.Size = new System.Drawing.Size(173, 23);
+            this.macroSelectionTB.TabIndex = 1;
+            // 
+            // macroSelectionLBL
+            // 
+            this.macroSelectionLBL.AutoSize = true;
+            this.macroSelectionLBL.Font = new System.Drawing.Font("Bahnschrift", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.macroSelectionLBL.Location = new System.Drawing.Point(217, 290);
+            this.macroSelectionLBL.Name = "macroSelectionLBL";
+            this.macroSelectionLBL.Size = new System.Drawing.Size(139, 18);
+            this.macroSelectionLBL.TabIndex = 2;
+            this.macroSelectionLBL.Text = "Selected Macro Key";
+            // 
+            // pressKeyLBL
+            // 
+            this.pressKeyLBL.AutoSize = true;
+            this.pressKeyLBL.Font = new System.Drawing.Font("Bahnschrift", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pressKeyLBL.Location = new System.Drawing.Point(392, 283);
+            this.pressKeyLBL.Name = "pressKeyLBL";
+            this.pressKeyLBL.Size = new System.Drawing.Size(480, 115);
+            this.pressKeyLBL.TabIndex = 3;
+            this.pressKeyLBL.Tag = "Overlay";
+            this.pressKeyLBL.Text = "Press Key";
+            this.pressKeyLBL.Visible = false;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.pressKeyLBL);
+            this.Controls.Add(this.macroSelectionLBL);
+            this.Controls.Add(this.macroSelectionTB);
+            this.Controls.Add(this.macroBTN);
             this.Name = "Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button macroBTN;
+        private System.Windows.Forms.TextBox macroSelectionTB;
+        private System.Windows.Forms.Label macroSelectionLBL;
+        private System.Windows.Forms.Label pressKeyLBL;
     }
 }
 
--- Macto/Main.cs
+++ Macto/Main.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Gma.System.MouseKeyHook;
 
 namespace Macto
 {
@@ -15,6 +16,52 @@ namespace Macto
         public Main()
         {
             InitializeComponent();
+        }
+
+        private IKeyboardMouseEvents globalHook;
+
+
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            globalHook.Dispose();
+            base.OnFormClosed(e);
+        }
+
+        private void macroBTN_Click(object sender, EventArgs e)
+        {
+            // Show "Press Key" overlay
+            foreach (Control ctrl in this.Controls)
+            {
+                ctrl.Visible = ctrl.Tag?.ToString() == "Overlay";
+            }
+
+            globalHook = Hook.GlobalEvents();
+
+            // Listen for keyboard clicks
+            globalHook.KeyDown += (s, keyEvent) =>
+            {
+                macroSelectionTB.Text = keyEvent.KeyCode.ToString();
+                FinishInput();
+            };
+
+            // Listen for mouse clicks
+            globalHook.MouseDown += (s, mouseEvent) =>
+            {
+                macroSelectionTB.Text = mouseEvent.Button.ToString();
+                FinishInput();
+            };
+
+            // Dispose of hook and hide "Press Key" overlay
+            void FinishInput()
+            {
+                globalHook.Dispose();
+                globalHook = null;
+
+                foreach (Control ctrl in this.Controls)
+                {
+                    ctrl.Visible = ctrl.Tag?.ToString() != "Overlay";
+                }
+            }
         }
     }
 }
